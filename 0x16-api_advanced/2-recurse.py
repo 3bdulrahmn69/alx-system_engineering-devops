18ba757d98a945987c7c2b21538d84d38a5b19f9
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-11-07 15:55:17 +0200

    0x16

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,7 +5,7 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[] , after='', count=0):
+def recurse(subreddit, hot_list=[], after='', count=0):
     """queries the Reddit API and returns a list containing the
     titles of all hot articles"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
